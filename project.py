b5c27edb0bcdc755d000bc9faa8f80409eedf55e
Meilan Chen <meilannihao@gmail.com>
2018-04-03 14:20:44 -0400

    Update project.py

--- project.py
+++ project.py
@@ -19,4 +19,25 @@ data_all = pd.read_csv("allData.tsv", sep='\t')
 #view data
 data_all.head()
 
+# Correlation matrix
+d = data_all
+
+# Compute the correlation matrix
+corr = d.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+
 ### test#######
