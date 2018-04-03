4df5629ba1ba2b96706b9d84a5a164dd2d197dc1
jcfeng1992 <37516401+jcfeng1992@users.noreply.github.com>
2018-04-03 14:25:33 -0400

    Update project.py

--- project.py
+++ project.py
@@ -40,4 +40,43 @@ sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
             square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
 
+#view data
+data_all.head()
+data_all.columns
+
+#analyze data
+
+data_all['age'].describe()
+data_all['gender'].describe()
+#histogram
+sns.distplot(data_all['rating'])
+#data_all.plot.scatter(x='age', y='rating')
+fig = sns.boxplot(x='age', y='rating',data=data_all)
+#correlation matrix
+corrmat = data_all.corr()
+sns.heatmap(corrmat, square=True);
+
+##scatterplot
+#sns.set()
+#sns.pairplot(data_all, size = 2.5)
+#plt.show();
+
+
+#missing data
+data_all.count()
+data_all.isnull().sum()
+
+# Create a set of dummy variables from the genre variable
+df_genre1 = pd.get_dummies(data_all['genre1'])
+df_genre2 = pd.get_dummies(data_all['genre2'])
+df_genre3 = pd.get_dummies(data_all['genre3'])
+
+df_genre = pd.concat([df_genre1,df_genre2,df_genre3]).groupby(level=0).any().astype(int)
+
+data_new = pd.concat([data_all[['userID', 'age', 'gender', 'movieID', 'name', 'year']], df_genre,data_all['rating']], axis=1)
+
+
+
+
+
 ### test#######
